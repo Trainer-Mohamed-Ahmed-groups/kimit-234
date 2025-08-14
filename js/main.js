8b8575446f90ab56626678fec86dfe06df61357a
mohamed-ahmed10 <eng.mohamedahmed59@gmail.com>
2025-08-14 20:52:45 +0300

    to conversion

--- js/main.js
+++ js/main.js
@@ -1,141 +1,50 @@
 'use strict';
 
-var student = ["John Doe", 20, "Computer Science", undefined, null, ["Math", "Physics"], { name: "John", age: 20 }, true, 'test'];
 
-// student[0] = "John Doe"
-// student[1] = 20
-// student[2] = "Computer Science"
+var x = 0;
 
-console.log(student)
-console.log(student[0])
-console.log(student[4])
-console.log(student[5][0])
-console.log(student[6].age)
-console.log(student[6]["name"])
-console.log(student.length)
-console.log(student[7])
-console.log(student[student.length - 1])
-console.log(student.at(-1))
-console.log(student[10])
-
-console.log("*********************************************")
-///////////////////////////////////////////////////////////
-
-var x = 5
-
-if (x !== '5') {
-    console.log("YES");
-}
-else {
-
-    console.log("NO")
+while (x <= 10) {
+    console.log(x);
+    x++;
 }
 
+console.log("**********************************************");
 
-var y = 12;
+var y = 0;
 
+do {
+    console.log(y);
+    y++;
+} while (y <= 10);
 
-console.log(y);
-console.log(y--);
-console.log(++y);
+console.log("**********************************************");
 
+for (var z = 1; z <= 10; z += 2) {
+    console.log(z);
+}
 
-console.log(y % 3)
+console.log("**********************************************");
 
-y += 15;
+console.log(x)
 console.log(y)
-
-console.log(y %= 3)
-console.log(y)
+console.log(z)
 
 
-if (!true) {
-    console.log("This is true");
+
+var persons = [
+    { name: 'John', age: 30 },
+    { name: 'Jane', age: 25 },
+    { name: 'Mike', age: 35 }
+];
+
+
+console.log("Hello (name) your age is (age)");
+
+
+console.log("**********************************************");
+
+
+for (var t = 0; t < 10; t++) {
+    if (t === 7) break;
+    console.log(t)
 }
-// 50 : 65  Pass
-// 65 : 75 Good
-// 75 : 85 Very Good
-// 85 : 100 Excellent
-
-function grade(num) {
-
-    if (num >= 0) {
-        if (num <= 100) {
-            if (num >= 85) console.log("Excellent");
-            else if (num >= 75) console.log("Very Good");
-            else if (num >= 65) console.log("Good");
-            else if (num >= 50) console.log("Pass");
-            else console.log("Fail");
-        }
-    }
-    else { }
-}
-
-
-grade(90);
-
-
-var t = 1
-if (t > 5 && t < 15) {
-    console.log("t is between 5 and 15");
-}
-
-
-if (t > 5 || t < 15) {
-    console.log("t is either greater than 5 or less than 15");
-}
-
-
-console.log("test" && 0 && "YES")
-// AND returns the first falsy value or the last truthy value
-
-
-
-console.log("" || NaN || null)
-// OR return the first truthy value or the last falsy value
-
-
-try {
-    console.lo(object)
-} catch (error) {
-    console.error("Dev ERROR : " + error.message)
-} finally {
-    console.log("This is the finally block, it always executes")
-}
-
-var t = 1;
-
-switch (t) {
-    case 2:
-    case 1:
-        console.log("t equal 1 OR 2");
-        break;
-    case 3:
-        console.log("t equal 3");
-        break;
-    default:
-        console.log("t is not equal to 1, 2, or 3");
-}
-
-
-function getRectArea(width, height) {
-    if (isNaN(width) || isNaN(height)) {
-        throw new Error("Invalid input: width and height must be numbers");
-    }
-    else return width * height;
-}
-
-// var width = +prompt("Enter width:");
-// var height = +prompt("Enter height:");
-
-try {
-    console.log(getRectArea(45, 10));
-} catch (error) {
-    console.error(error);
-}
-
-
-
-p = 8;
-
-console.log(p);
