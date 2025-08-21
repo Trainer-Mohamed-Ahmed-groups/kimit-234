34c715bdc3b2fa08f8f8d4c17c905c8438105030
mohamed-ahmed10 <eng.mohamedahmed59@gmail.com>
2025-08-21 20:49:01 +0300

    To Date

--- js/main.js
+++ js/main.js
@@ -1,75 +1,99 @@
 'use strict';
+var fruits = ["Mongo", "Banana", "apple", "Strawberry"],
+    numbers = [10, 20, 30, 40, 50, 60, 70]
 
 
-var jobTitle = "I am a frontend developeR and Frontend instructoR",
-    username = "ALI";
-
-console.log(jobTitle.concat(" This is " + username))
-console.log(jobTitle.length)
-console.log(username[2])
-console.log(username.charAt(1))
-console.log(username.charCodeAt(2))
-console.log(String.fromCharCode(122))
+console.log(fruits)
+console.log(numbers)
+console.log(fruits.length)
+console.log(fruits.at(2))
 
 
-console.log(jobTitle.startsWith('I'));
-console.log(jobTitle.endsWith('I'));
-console.log(jobTitle.includes('r'))
-console.log(jobTitle.indexOf('r'))
-console.log(jobTitle.indexOf('z')) // -1
-console.log(jobTitle.lastIndexOf('r'))
-console.log(jobTitle.lastIndexOf('z')) // -1
+console.log(fruits.join(' * '))
 
 
-console.log(jobTitle.match(/r/ig))
-console.log(jobTitle.match(/r/ig).length)
+console.log(numbers.some(function (number) { return number > 25 }))
+console.log(numbers.some((number) => number > 25))
+console.log(numbers.some(checkAges))
 
+
+function checkAges(age) {
+    return age > 25
+}
+
+console.log(numbers.every(number => number > 25))
+
+// console.log(fruits.some(sample => sample.startsWith('A')));
 
 console.log("*".repeat(20))
 
-
-console.log(jobTitle.replace(/front/ig, "back"))
-
-console.log(jobTitle.search('a'))
-
-console.log(jobTitle.split(' '))
-console.log(jobTitle.split(' ').join("-"))
+console.log(numbers.find(number => number > 20))
+console.log(numbers.findIndex(number => number > 20))
 
 
-console.log(jobTitle.slice(2, 8))
+console.log(numbers.filter(number => number > 20))
+console.log("*".repeat(20))
 
 
-console.log(jobTitle.substring(2, 8))
-console.log(jobTitle.substr(2, 8))
+var test = numbers.map(number => {
+    if (number > 20) return "+"
+    else return "-"
+})
+
+var test2 = numbers.forEach(number => console.log(number))
 
 
-console.log(jobTitle.toUpperCase())
-console.log(jobTitle.toLocaleLowerCase())
+console.log(test)
+console.log(test2)
+console.log(numbers)
 
 
-var newText = "        I      m     test     "
-console.log(newText)
-console.log(newText.trimStart())
-console.log(newText.trimEnd())
-console.log(newText.trim())
+console.log(fruits.slice(1, 2))
+console.log(fruits.slice(1))
+console.log(fruits.slice(0, 2))
+
+console.log("*".repeat(20))
+
+// console.log(numbers.fill(33, 3, 5))
+
+// numbers.push(100)
+// numbers.pop()
+
+// numbers.shift()
+// numbers.unshift('test')
+
+// console.log(numbers.splice(1, 3))
+console.log(numbers.splice(1, 3, 'new Item', 'new Item2', 'new Item3'))
+// console.log(numbers.toSpliced(1, 3))
+
+console.log(numbers)
 
 
-// Math
-
-console.log(Math.PI)
-console.log(Math.abs(-8))
-console.log(Math.abs(8))
-console.log(Math.round(3.7))
-console.log(Math.round(3.3))
-console.log(Math.ceil(9.1))
-console.log(Math.floor(9.9))
+console.log(fruits.sort())
 
 
-var myNumbers = [54, 4, 78, 52, 5, 78, 78, 7]
-console.log(myNumbers)
-console.log(...myNumbers)
-console.log(Math.max(...myNumbers))
-console.log(Math.min(...myNumbers))
-console.log(Math.pow(2, 3))
-console.log(Math.random() * 10)
-console.log(Math.round(Math.random() * 10))
+
+
+var numbers2 = [91, 21, 201, 10, 101]
+console.log(numbers2.sort((a, b) => a - b))
+
+let i = 1
+function sayHello() {
+    console.log("Hello : " + i)
+    i++
+}
+
+
+var myInterval = setInterval(sayHello, 500)
+// var myTimeout = setTimeout(sayHello, 5000)
+
+
+function stop() {
+    clearInterval(myInterval)
+    // clearInterval(myTimeout)
+}
+
+
+var myDate = new Date("25 Mar 2015");
+
+console.log(myDate)
